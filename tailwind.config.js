dbbf450ceb5123ff1e18efcb7310736f96f5a79b
webapp99 <webapp99tube@gmail.com>
2022-03-08 13:43:48 +0600

    Portfolio website with tailwindcss 3

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#A3A3FF',
+        'theme': '#3F3FFF',
+        'nav': '#404053',
+        'secondary': '#919144',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
